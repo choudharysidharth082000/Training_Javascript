5edc4083d7d8bcaa5d06abe2ec13e10513eb88dd
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2023-01-03 16:11:10 +0530

    Added Some more files

---
+++ functions.js
@@ -0,0 +1,57 @@
+//there are two types of functions
+
+//1. Call By Value Functions
+//2. Call By Reference Functions
+
+//examples
+//1. Call By Value Functions
+//1.1 Paramterarized Functions
+//1.2 Non Paramterarized Functions
+
+
+const sampleFunction = () => // this is a non paramterarized function
+{
+    console.log("Hello world")
+}
+
+
+
+
+
+//this function is also call by value function as another copy of the paramter is created and the function is called
+
+//call by value functions is used for the variables
+var b =12;
+console.log("Before the function call", b);
+const sampleFunction2 = (a) =>
+{
+    a= a+a;
+    console.log(a);
+}
+sampleFunction2(b);
+console.log("After the function is called", b);
+
+
+
+
+
+
+
+
+
+
+
+
+//call by reference functions   only possible in arrays and objects in javascript
+var sampleObject = 
+{
+    a : 1
+}
+console.log("Before the function call", sampleObject);
+const sampleFuncton3 = (samplea) => // this is a call by reference function
+{
+    samplea.a = samplea.a + samplea.a;
+}
+sampleFuncton3(sampleObject);
+console.log("After the function is called");
+console.log(sampleObject);
