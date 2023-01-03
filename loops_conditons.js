653323918ec96379a7303a4aeb18cc6b6103c7ef
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2023-01-03 14:31:10 +0530

    Added Loops and Conditions

---
+++ loops_conditons.js
@@ -0,0 +1,15 @@
+//loop and condition examples
+
+//for loop example
+for(let a = 0;a<5;a++){
+    console.log(a);
+}
+
+//while loop example
+
+var a = 20;
+console.log("While Loop example");
+while(a <=30){
+    console.log(a++);
+}
+
