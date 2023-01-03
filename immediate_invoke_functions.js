7bf9109999e80099ed6d19d3394abef6a849ea53
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2023-01-03 17:00:21 +0530

    Made some changes to the function invoccation modeule

--- immediate_invoke_functions.js
+++ immediate_invoke_functions.js
@@ -1,4 +1,14 @@
 //example for immediate function invocation
+
+//used for optimization 
+
+//used the function and frees the memory after the function is executed
+
+//if we decakre the function in a global space then throughout the program our space will be taken by the variable
+
+//if it requires urage in single function only then we can use the immediate function invocation
 (function(){
+    let a =10;
+    console.log(a); 
     console.log("This is the immediate function invocation");
 })();
