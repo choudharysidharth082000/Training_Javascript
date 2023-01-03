8e79dbf476af152d0f71190250915b7068349a07
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2023-01-03 17:31:55 +0530

    Added some more files

---
+++ callbacks.js
@@ -0,0 +1,18 @@
+
+
+//javascript is by defaut the single threaded synchronous language and to make it asynchronous we use the callbacks
+setTimeout(()=>
+{
+    console.log("TimeOut Number");
+}, 5000);
+
+
+const x = (y) =>
+{
+    console.log("x");
+    y();
+}
+
+x(function(){ //passsing the callbacks here
+    console.log("This is the inner function");
+})
