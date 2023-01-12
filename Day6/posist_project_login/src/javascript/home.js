ebbf8d8432bc0411fc2b8dbbf39550ca2be64c16
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2023-01-12 16:27:48 +0530

    Added Poster rows

--- Day6/posist_project_login/src/javascript/home.js
+++ Day6/posist_project_login/src/javascript/home.js
@@ -1,10 +1,11 @@
 console.log("Hello world");
 
 const logoutButton = document.getElementById("logout");
-const banner = document.getElementById("containorBackdrop");    
-const bannerHeader = document.getElementById("headerWelcome")
-const bannerDescription = document.getElementById("bannerDiscription")
+const banner = document.getElementById("containorBackdrop");
+const bannerHeader = document.getElementById("headerWelcome");
+const bannerDescription = document.getElementById("bannerDiscription");
 const containorShows = document.getElementById("containorShows");
+const containorShowsAction = document.getElementById("containorShowsAction");
 console.log(bannerHeader);
 
 logoutButton.addEventListener("click", (e) => {
@@ -14,11 +15,9 @@ logoutButton.addEventListener("click", (e) => {
 });
 
 //string truncate method
-const  truncate = (string,n) =>
-{
-    return string?.length > n ? string.substr(0,n-1) + ' ...': string;
-
-}
+const truncate = (string, n) => {
+  return string?.length > n ? string.substr(0, n - 1) + " ..." : string;
+};
 
 window.addEventListener("load", (event) => {
   if (
@@ -37,28 +36,48 @@ window.addEventListener("load", (event) => {
       return response.json();
     })
     .then((data) => {
-        console.log(data.results);
+      console.log(data.results);
       //adding the background image to the the containor div
-      const randomNumber = Math.floor(Math.random()*data.results.length-1);
+      const randomNumber = Math.floor(Math.random() * data.results.length - 1);
       banner.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${data.results[randomNumber].backdrop_path})`;
       //changing the content in the benner Header
       bannerHeader.innerHTML = data.results[randomNumber].title;
       bannerDescription.innerHTML = data.results[randomNumber].overview;
 
       //creating the cards
-        data.results.forEach((movie) => {
-            const card = document.createElement("div");
-            card.classList.add("containorPoster");
-            card.classList.add("max-height-poster")
-            card.innerHTML = `
+      data.results.forEach((movie) => {
+        const card = document.createElement("div");
+        card.classList.add("containorPoster");
+        card.classList.add("max-height-poster");
+        card.innerHTML = `
             <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="card-img-top" alt="...">
             `;
-            containorShows.appendChild(card);
-        });
-
-
+        containorShows.appendChild(card);
+      });
     })
     .catch((err) => {
       console.log(err.message);
+    });
+
+  //action url
+  const actionURL =
+    " https://api.themoviedb.org/3/discover/movie?api_key=94f9f5533f279def2c111bf737c3e9a9&with_genres=27";
+  const getActionData = fetch(actionURL);
+  getActionData
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.results);
+      //creating the cards
+      data.results.forEach((movie) => {
+        const card = document.createElement("div");
+        card.classList.add("containorPoster");
+        card.classList.add("max-height-poster");
+        card.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="card-img-top" alt="...">
+                `;
+        containorShowsAction.appendChild(card);
+      });
     });
 });
